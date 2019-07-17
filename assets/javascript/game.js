cc6370704b3138935deec425631152f15d50731a
Dardan Bahtijari <dardan.bahtijari@yahoo.com>
2019-07-17 12:37:45 -0400

    Added Stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var randomNumber;
+var winner;
+var loser;
+
+
+randomNumber = Math.floor(Math.random() * 101) + 19;
+
+
+for (var i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 12);
+
+}
+
+ console.log(random);
